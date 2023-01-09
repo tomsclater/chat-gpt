5f81e636c489adfe709a64f7f0285c602b446a58
Thomas Sclater <tomsclater@live.com>
2023-01-08 20:48:13 -0800

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
 import './normal.css';
+import { FaPlus } from 'react-icons/fa';
 
 function App() {
   return (
     <div className="App">
       <aside className="sidemenu">
-        <h1>Aside</h1>
+        <div className="side-menu-button">
+        <FaPlus className="plus-button"/>
+        New chat  
+        </div>
       </aside>
       <section className="chatbox">
       
