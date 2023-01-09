881e6b9870a76b0b62b9cad38de27e8dcf35c6b0
Thomas Sclater <tomsclater@live.com>
2023-01-08 21:40:06 -0800

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,20 @@ function App() {
       </aside>
       <section className="chatbox">
         <h1 className="interface-heading">AI Chat Interface</h1>
+        <div class="parent">
+          <div class="div1"> 1</div>
+          <div class="div2"> 2</div>
+          <div class="div3"> 3</div>
+          <div class="div4"> 4</div>
+          <div class="div5"> 5</div>
+          <div class="div6"> 6</div>
+          <div class="div7"> 7</div>
+          <div class="div8"> 8</div>
+          <div class="div9"> 9</div>
+          <div class="div10"> 10</div>
+          <div class="div11"> 11</div>
+          <div class="div12"> 12</div>
+        </div>
         <div className="chat-input-holder">
           <div>
           <textarea
