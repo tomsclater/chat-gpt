016d9ce7a2411808e934733c1e18ba2bee307c24
Thomas Sclater <tomsclater@live.com>
2023-01-11 10:22:38 -0800

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -100,13 +100,13 @@ function App() {
           <div class="div1"> <BsSun/><br/>Examples</div>
           <div class="div2"> <AiOutlineThunderbolt/><br/>Capabilities</div>
           <div class="div3"> <CiWarning/><br/>Limitations</div>
-          <div class="div4"> "Explain quantum computing in simple terms"</div>
+          <div class="div4"> "Explain quantum computing in simple terms" →</div>
           <div class="div5"> Remembers what user said earlier in the conversation</div>
           <div class="div6"> May occasionally generate incorrect information</div>
-          <div class="div7"> "Got any creative ideas for a 10 year old's birthday?"</div>
+          <div class="div7"> "Got any creative ideas for a 10 year old's birthday?" →</div>
           <div class="div8"> Allows user to provide follow-up corrections</div>
           <div class="div9"> May occasionally produce harmful instructions or biased content</div>
-          <div class="div10"> "How do I make an HTTP request in JavaScript?"</div>
+          <div class="div10"> "How do I make an HTTP request in JavaScript?" →</div>
           <div class="div11"> Trained to decline inappropriate requests</div>
           <div class="div12"> Limited knowledge of world and events after 2021</div>
         </div>
